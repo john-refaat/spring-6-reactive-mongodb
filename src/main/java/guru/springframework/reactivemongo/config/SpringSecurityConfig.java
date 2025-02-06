6336a914f38d40c0e052881c18b6cad1b7fe461a
John <john.refaat88@gmail.com>
2025-02-06 05:43:01 +0200

    Adding Spring Actuator

--- src/main/java/guru/springframework/reactivemongo/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/reactivemongo/config/SpringSecurityConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.reactivemongo.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -16,6 +18,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(1)
+    SecurityWebFilterChain actuatorWebFilterChain(ServerHttpSecurity http) {
+        return http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(auth -> auth.anyExchange().permitAll()).build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
